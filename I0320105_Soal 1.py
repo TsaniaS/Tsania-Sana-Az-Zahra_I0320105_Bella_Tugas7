1bd306f8c5475e9cd8dacd0fb28ed9fb02e6c903
Tsania Sana <tsania.sana@gmail.com>
2021-04-16 20:57:58 -0800

    Tugas 7

---
+++ I0320105_Soal 1.py
@@ -0,0 +1,16 @@
+# SOAL 1
+# fungsi capitalize()
+teks = "selamat mengerjakan tugas."
+a = teks.capitalize()
+print(a)
+print(" ")
+
+#fungsi center()
+teks = "Marhaban Ya Ramadhan"
+a = teks.center(40,"-")
+print(a)
+print(" ")
+
+#Fungsi count()
+teks = "berapa banyak huruf a?"
+print(teks.count("a"))
---
+++ I0320105_Soal 2.py
@@ -0,0 +1,22 @@
+# SOAL 2
+# Fungsi abs()
+x = -8
+y = -1.679
+print(abs(x))
+print(abs(y))
+print(" ")
+
+# Fungsi min() dan max()
+a = 0.9
+b = 90
+c = 9
+print("nilai min = ", min(a,b,c))
+print("nilai max = ", max(a,b,c))
+print(" ")
+
+# Fungsi ceil() math
+import math
+a = 5.7
+b = 2.5
+print(math.ceil(a))
+print(math.ceil(b))
